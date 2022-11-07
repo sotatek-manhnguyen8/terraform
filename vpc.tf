e70f856c32bb2c4f4807f6df4df6b537a8839fa9
Your Name <you@example.com>
2022-11-07 13:20:00 +0000

    add subnet

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,21 @@ resource "aws_vpc" "main" {
   }
 }
 
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/20"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "main1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/20"
+
+  tags = {
+    Name = "Main"
+  }
+}
